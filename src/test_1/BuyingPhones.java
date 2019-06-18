f2fba2b68f636c78598a97968b4e94c6c997fe87
747624 <747624@123-PC31.student.Tor.Loc>
2019-06-18 16:11:35 -0400

    initial commit

---
+++ src/test_1/BuyingPhones.java
@@ -0,0 +1,19 @@
+package test_1;
+
+import java.util.Scanner;
+
+public class BuyingPhones {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the regular price of a phone: ");
+		Scanner s = new Scanner(System.in);
+		double price = s.nextDouble();
+		double price2=price/2;
+		System.out.println("regular price for phoneprice  " +price);
+		System.out.println(" no. of phones is 3");
+		System.out.println(" phone 1: "+price);
+		System.out.println(" phone 2: "+price);
+		System.out.println(" phone 3: "+price2);
+	}
+
+}
---
+++ src/test_1/SubstringForKids.java
@@ -0,0 +1,20 @@
+package test_1;
+
+public class SubstringForKids {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SUBSTRING FOR KIDS");
+		
+		
+		
+		
+
+	}
+	
+	public String substringForKids() {
+		return "";
+	}
+	
+
+}
